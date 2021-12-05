2db8812fbaaf6a10cbdc30dde06be80f336692e5
Titouan Bénard <itotutona@evta.fr>
2021-12-05 01:02:17 +0100

    Add yaml config

---
+++ nft_generator/config/c_yaml.py
@@ -0,0 +1,8 @@
+import yaml
+
+from nft_generator.config import BaseConfig
+
+class YamlConfig(BaseConfig):
+    def parse_config(self):
+        self.config = yaml.safe_load(self.input_config)
+        
