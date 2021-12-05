16caca99327e8ce4933c97124cce1bf63ac1a143
Titouan Bénard <itotutona@evta.fr>
2021-12-05 01:02:04 +0100

    Add toml config

---
+++ nft_generator/config/c_toml.py
@@ -0,0 +1,9 @@
+import toml
+
+from nft_generator.config import BaseConfig
+
+class TomlConfig(BaseConfig):
+    def parse_config(self):
+        self.config = toml.loads(self.input_config)
+        
+
