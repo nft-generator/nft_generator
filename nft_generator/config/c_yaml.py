6f9b6da5bb5640164995019b8a8abb713bf8882c
Titouan Bénard <itotutona@evta.fr>
2021-12-09 18:27:50 +0100

    Update yaml config

--- nft_generator/config/c_yaml.py
+++ nft_generator/config/c_yaml.py
@@ -5,4 +5,4 @@ from nft_generator.config import BaseConfig
 class YamlConfig(BaseConfig):
     def parse_config(self):
         self.config = yaml.safe_load(self.input_config)
-        
+        return self.config
