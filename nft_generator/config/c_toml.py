20c704307fd19167db777d143b16cc3d1472a154
Titouan Bénard <itotutona@evta.fr>
2021-12-09 18:27:39 +0100

    Update config

--- nft_generator/config/c_toml.py
+++ nft_generator/config/c_toml.py
@@ -5,5 +5,5 @@ from nft_generator.config import BaseConfig
 class TomlConfig(BaseConfig):
     def parse_config(self):
         self.config = toml.loads(self.input_config)
-        
+        return self.config
 
