731b7ca7a022eab5ddfa6bc47ef9bc19e5411511
Titouan Bénard <itotutona@evta.fr>
2021-12-09 18:27:23 +0100

    Add new command

--- nft_generator/cli.py
+++ nft_generator/cli.py
@@ -8,8 +8,10 @@ def main():
     pass
 
 from nft_generator.command.version import version
+from nft_generator.command.generate import generate
 
 main.add_command(version)
+main.add_command(generate)
 
 if __name__ == "__main__":
     main()
