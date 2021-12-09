be16d1dc59c00d091246473da0600f5ca5b4cce7
Titouan Bénard <itotutona@evta.fr>
2021-12-09 09:44:31 +0100

    Add load_config_from_file

--- nft_generator/config/__init__.py
+++ nft_generator/config/__init__.py
@@ -4,3 +4,12 @@ from nft_generator.config.base import BaseConfig
 from nft_generator.config.c_toml import TomlConfig
 from nft_generator.config.c_json import JsonConfig
 from nft_generator.config.c_yaml import YamlConfig
+
+def load_config_from_file(filename:str):
+    ext = filename.split(".")[1]
+    if ext == "yml" or ext == "yaml":
+        return YamlConfig.load_config_from_file(filename)
+    elif ext == "toml":
+        return TomlConfig.load_config_from_file(filename)
+    elif ext == "json":
+        return JsonConfig.load_config_from_file(filename)
