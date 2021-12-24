e2e17a204054a39eaf5d904d0956542e11155360
Titouan Bénard <itotutona@evta.fr>
2021-12-24 01:09:05 +0100

    Update test

--- tests/test_nft_generator.py
+++ tests/test_nft_generator.py
@@ -13,24 +13,9 @@ import pytest
 from contextlib import contextmanager
 from click.testing import CliRunner
 
-from nft_generator import nft_generator
 from nft_generator import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
 def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
